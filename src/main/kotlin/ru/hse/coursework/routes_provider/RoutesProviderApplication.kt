b678d62c88e8fea235a63550e7bbc1fc08841415
Alex <yevsyukovaleks@gmail.com>
2024-11-23 01:07:00 +0300

    Initial commit

---
+++ src/main/kotlin/ru/hse/coursework/routes_provider/RoutesProviderApplication.kt
@@ -0,0 +1,11 @@
+package ru.hse.coursework.routes_provider
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoutesProviderApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoutesProviderApplication>(*args)
+}
---
+++ src/test/kotlin/ru/hse/coursework/routes_provider/RoutesProviderApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.hse.coursework.routes_provider
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoutesProviderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
